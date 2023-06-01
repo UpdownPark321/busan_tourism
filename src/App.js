1a224e33e9e2e51798dabd8f68dd221d89aaa30f
SongMinQQ <107177636+SongMinQQ@users.noreply.github.com>
2023-06-01 14:48:38 +0900

    공공데이터를 Information 컴포넌트로 props 형태로 넘겨줌

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.css';
 import Information from './components/Information'; 
 import Question from './components/Question'; 
-import Main from './components/Main'; 
+import Main from './components/Main';
+import axios from 'axios';
 
 // import busanImg from './project/0.jpg';
 
 const App = () => {
-  const [index, setIndex] = React.useState("main");
+  const [index, setIndex] = useState("main");
+  const [data,setData] = useState([]);
+
     const onSelect = (event) => {
       setIndex(event.target.value);
+      axios.get("http://localhost:3001/busan")//메뉴 선택 이벤트 수행 시 데이터를 받아옴, Information 컴포넌트에 데이터를 props로 넘겨줌
+      .then((res) => {
+        setData([...res.data.getAttractionKr.item]);
+        console.log(res);
+      }).catch((err) => {
+          console.log(err);
+      })
     }
+
 
   return(
     <div>
@@ -25,7 +36,7 @@ const App = () => {
       </header>
       <hr />
       {index === "main" ? <Main /> : null}
-      {index === "0" ? <Information /> : null}
+      {index === "0" ? <Information data={data}/> : null}
       {index === "1" ? <Question /> : null}
     
   </div>
--- src/components/Information.js
+++ src/components/Information.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Information = () => {
+const Information = (props) => {
+  console.log(props);
   return (
     <div className="Information">
     
