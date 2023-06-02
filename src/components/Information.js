0f83e1b5c91bf0bec8001e418d94487df3a52dd4
UpdownPark321 <psangh7070@gmail.com>
2023-06-02 13:39:00 +0900

    Information page 반응형 UI

--- src/components/Information.js
+++ src/components/Information.js
@@ -7,7 +7,7 @@ const Information = (props) => {
     return data.map((item) => (
       <div className="tour-spot" key={item.MAIN_TITLE}>
         <h3>{item.MAIN_TITLE}</h3>
-        <img src={item.MAIN_IMG_NORMAL} width='230' height='150' alt={item.MAIN_TITLE} className='leftArea'></img>
+        <img src={item.MAIN_IMG_NORMAL}  alt={item.MAIN_TITLE} className='leftArea'></img>
         <div className='rightArea'>
           <span>{item.SUBTITLE?item.SUBTITLE:item.TITLE}</span>
         </div>
@@ -17,15 +17,11 @@ const Information = (props) => {
 
   return (
     <div className="Information">
-    
-      <main>
         <section className="content">
-      
           <div className="tour-spots">
             {makeJsxEle(props.data)}
           </div>
         </section>
-      </main>
     </div>
     
   );
