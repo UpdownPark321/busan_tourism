7bcd2fd2b73b9ceb2115e1096b9185022a28f466
UpdownPark321 <psangh7070@gmail.com>
2023-06-06 23:22:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ import Login from './components/Login';
 
 
 const App = () => {
-  const [index, setIndex] = useState("main");
+  const [page, setPage] = useState("main");
   const [data,setData] = useState([]);
 
     const onSelect = (event) => {
-      setIndex(event.target.value);
+      setPage(event.target.value);
       axios.get("http://localhost:3001/busan")//메뉴 선택 이벤트 수행 시 데이터를 받아옴, Information 컴포넌트에 데이터를 props로 넘겨줌
       .then((res) => {
         setData([...res.data.getAttractionKr.item]);
@@ -28,17 +28,17 @@ const App = () => {
     <div>
       <header className="header">
         <h1>Busan Tourism</h1>
-        <select  className="select" value={index} onChange={onSelect}>
+        <select  className="select" value={page} onChange={onSelect}>
           <option value="main">Main Page</option>
           <Login/>
-          <option value="0">Information</option>
-          <option value="1">Q & A</option>
+          <option value="info">Information</option>
+          <option value="gpt">Q & A</option>
       </select>
       </header>
      
-      {index === "main" ? <Main /> : null}
-      {index === "0" ? <Information data={data}/> : null}
-      {index === "1" ? <Gpt /> : null}
+      {page === "main" && <Main />}
+      {page === "info" && <Information data={data}/> }
+      {page === "gpt" && <Gpt />}
     
   </div>
   );
